02bdf732f937f16e4fc73cd8d333613e80d7c880
Snowlance <snowlance7@gmail.com>
2024-04-29 14:48:02 -0500

    figure out adding script and readd components to prefab

--- SCP956.cs
+++ SCP956.cs
@@ -1,6 +1,8 @@
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using System.IO;
+using System.Reflection;
 using UnityEngine;
 
 namespace SCP956
@@ -28,7 +30,24 @@ namespace SCP956
 
             LoggerInstance = PluginInstance.Logger;
 
+            // Configs
 
+            // Loading Assets
+            string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            DNAssetBundle = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "mod_assets"));
+            LoggerInstance.LogDebug($"Got DNAssetBundle at: {Path.Combine(sAssemblyLocation, "mod_assets")}");
+            if (DNAssetBundle == null)
+            {
+                LoggerInstance.LogError("Failed to load custom assets.");
+                return;
+            }
+
+            // Getting enemy
+            EnemyType SCP956 = DNAssetBundle.LoadAsset<EnemyType>("Assets/ModAssets/SCP-956/956.prefab");
+            LoggerInstance.LogDebug($"Got SCP-956 prefab: {SCP956}");
+
+            // SCP956AI scp956aiScript = SCP956.enemyPrefab.AddComponent<SCP956AI>(); //TODO: figure this out, also need to go into unity editor and add the correct components to the prefab
 
             harmony.PatchAll();
 
---
+++ SCP956AI.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCP956
+{
+    internal class SCP956AI
+    {
+    }
+}
