c643474497708056d38d8957c36d572e6c7b325a
Snowlance <snowlance7@gmail.com>
2024-05-11 22:18:08 -0500

    wtf is actually happening...cant change rotation in the start() method because it only changes sometimes depending on where it spawns...

--- SCP956.cs
+++ SCP956.cs
@@ -1,6 +1,9 @@
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
+using LethalLib.Modules;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using UnityEngine;
@@ -19,7 +22,21 @@ namespace SCP956
         public static ManualLogSource LoggerInstance;
         private readonly Harmony harmony = new Harmony(modGUID);
 
-        public static AssetBundle? DNAssetBundle;
+        public static AssetBundle? ModAssets;
+
+        // Rarity Configs
+        public static ConfigEntry<int> configExperimentationLevelRarity;
+        public static ConfigEntry<int> configAssuranceLevelRarity;
+        public static ConfigEntry<int> configVowLevelRarity;
+        public static ConfigEntry<int> configOffenseLevelRarity;
+        public static ConfigEntry<int> configMarchLevelRarity;
+        public static ConfigEntry<int> configRendLevelRarity;
+        public static ConfigEntry<int> configDineLevelRarity;
+        public static ConfigEntry<int> configTitanLevelRarity;
+        public static ConfigEntry<int> configModdedLevelRarity;
+        public static ConfigEntry<int> configOtherLevelRarity;
+
+        
 
         private void Awake()
         {
@@ -30,28 +47,84 @@ namespace SCP956
 
             LoggerInstance = PluginInstance.Logger;
 
+            harmony.PatchAll();
+
+            InitializeNetworkBehaviours();
+
             // Configs
+            // Rarity
+            configExperimentationLevelRarity = Config.Bind("Rarity", "ExperimentationLevelRarity", 500, "Experimentation Level Rarity");
+            configAssuranceLevelRarity = Config.Bind("Rarity", "AssuranceLevelRarity", 40, "Assurance Level Rarity");
+            configVowLevelRarity = Config.Bind("Rarity", "VowLevelRarity", 20, "Vow Level Rarity");
+            configOffenseLevelRarity = Config.Bind("Rarity", "OffenseLevelRarity", 30, "Offense Level Rarity");
+            configMarchLevelRarity = Config.Bind("Rarity", "MarchLevelRarity", 20, "March Level Rarity");
+            configRendLevelRarity = Config.Bind("Rarity", "RendLevelRarity", 50, "Rend Level Rarity");
+            configDineLevelRarity = Config.Bind("Rarity", "DineLevelRarity", 25, "Dine Level Rarity");
+            configTitanLevelRarity = Config.Bind("Rarity", "TitanLevelRarity", 33, "Titan Level Rarity");
+            configModdedLevelRarity = Config.Bind("Rarity", "ModdedLevelRarity", 30, "Modded Level Rarity");
+            configOtherLevelRarity = Config.Bind("Rarity", "OtherLevelRarity", 30, "Other Level Rarity");
+
+            // TODO: Add NULL checks
 
             // Loading Assets
             string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-            DNAssetBundle = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "mod_assets"));
-            LoggerInstance.LogDebug($"Got DNAssetBundle at: {Path.Combine(sAssemblyLocation, "mod_assets")}");
-            if (DNAssetBundle == null)
+            ModAssets = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Info.Location), "mod_assets"));
+            if (ModAssets == null)
             {
-                LoggerInstance.LogError("Failed to load custom assets.");
+                Logger.LogError($"Failed to load custom assets.");
                 return;
             }
+            LoggerInstance.LogDebug($"Got AssetBundle at: {Path.Combine(sAssemblyLocation, "mod_assets")}");
 
             // Getting enemy
-            EnemyType SCP956 = DNAssetBundle.LoadAsset<EnemyType>("Assets/ModAssets/SCP-956/956.prefab");
-            LoggerInstance.LogDebug($"Got SCP-956 prefab: {SCP956}");
+            EnemyType SCP956 = ModAssets.LoadAsset<EnemyType>("Assets/ModAssets/SCP-956/956.asset"); // TODO: Wont let me change rotation or anything in the unity editor
+            if (SCP956 == null) { LoggerInstance.LogError("Error: Couldnt get enemy from assets"); return; }
+            LoggerInstance.LogDebug($"Got SCP-956 prefab");
+            //var ExampleEnemyTN = ModAssets.LoadAsset<TerminalNode>("ExampleEnemyTN");
+            //var ExampleEnemyTK = ModAssets.LoadAsset<TerminalKeyword>("ExampleEnemyTK");
 
-            // SCP956AI scp956aiScript = SCP956.enemyPrefab.AddComponent<SCP956AI>(); //TODO: figure this out, also need to go into unity editor and add the correct components to the prefab
+            
 
-            harmony.PatchAll();
+            LoggerInstance.LogDebug("Settings rarities");
+            var SCP956LevelRarities = new Dictionary<Levels.LevelTypes, int> {
+                {Levels.LevelTypes.ExperimentationLevel, configExperimentationLevelRarity.Value},
+                {Levels.LevelTypes.AssuranceLevel, configAssuranceLevelRarity.Value},
+                {Levels.LevelTypes.VowLevel, configVowLevelRarity.Value},
+                {Levels.LevelTypes.OffenseLevel, configOffenseLevelRarity.Value},
+                {Levels.LevelTypes.MarchLevel, configMarchLevelRarity.Value},
+                {Levels.LevelTypes.RendLevel, configRendLevelRarity.Value},
+                {Levels.LevelTypes.DineLevel, configDineLevelRarity.Value},
+                {Levels.LevelTypes.TitanLevel, configTitanLevelRarity.Value},
+                {Levels.LevelTypes.All, configOtherLevelRarity.Value},
+                {Levels.LevelTypes.Modded, configModdedLevelRarity.Value},
+            };
+
+            LoggerInstance.LogDebug("Registering enemy network prefab...");
+            NetworkPrefabs.RegisterNetworkPrefab(SCP956.enemyPrefab);
+
+            LoggerInstance.LogDebug("Registering enemy...");
+            Enemies.RegisterEnemy(SCP956, SCP956LevelRarities);
 
             Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+        }
+
+        private static void InitializeNetworkBehaviours()
+        {
+            // See https://github.com/EvaisaDev/UnityNetcodePatcher?tab=readme-ov-file#preparing-mods-for-patching
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                foreach (var method in methods)
+                {
+                    var attributes = method.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false);
+                    if (attributes.Length > 0)
+                    {
+                        method.Invoke(null, null);
+                    }
+                }
+            }
         }
     }
 }
--- SCP956AI.cs
+++ SCP956AI.cs
@@ -1,10 +1,168 @@
-﻿using System;
+﻿using BepInEx.Logging;
+using GameNetcodeStuff;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using GameNetcodeStuff;
+using Unity.Netcode;
+using UnityEngine;
 
 namespace SCP956
 {
-    internal class SCP956AI
+    public class SCP956AI : EnemyAI
     {
+        //#pragma warning disable 0649
+        //public Transform turnCompass = null!;
+        //public Transform attackArea = null!;
+        //#pragma warning restore 0649
+
+        private static ManualLogSource logger = SCP956.LoggerInstance;
+        private float timeSinceHittingLocalPlayer;
+        private Vector3 positionRandomness;
+        private System.Random enemyRandom;
+        private bool isDeadAnimationDone;
+
+        enum State
+        {
+            SearchingForPlayer,
+            AttackingPlayers
+        }
+
+        public override void Start()
+        {
+            logger.LogDebug("Start()");
+            logger.LogDebug("Pinata spawned!");
+            transform.rotation = Quaternion.Euler(270, 0, 0); // TODO: THIS ONLY WORKS SOMETIMES IDK WHY TF IT DOES THIS FUCK THIS SHIT I AM BEYOND IRRITATED, only works in certain areas????
+            base.Start();
+
+            /*timeSinceHittingLocalPlayer = 0;
+            positionRandomness = new Vector3(0, 0, 0);
+            enemyRandom = new System.Random(StartOfRound.Instance.randomMapSeed + thisEnemyIndex);
+            isDeadAnimationDone = false;
+            // NOTE: Add your behavior states in your enemy script in Unity, where you can configure fun stuff
+            // like a voice clip or an sfx clip to play when changing to that specific behavior state.
+            ////currentBehaviourStateIndex = (int)State.SearchingForPlayer;
+            // We make the enemy start searching. This will make it start wandering around.
+            StartSearch(transform.position);*/
+        }
+
+        public override void Update()
+        {
+
+            //transform.rotation = Quaternion.Euler(270, 0, 0); // this fucking works of course, what the fuck
+            return;
+            base.Update();
+            return;
+            if (isEnemyDead)
+            {
+                // For some weird reason I can't get an RPC to get called from HitEnemy() (works from other methods), so we do this workaround. We just want the enemy to stop playing the song.
+                if (!isDeadAnimationDone)
+                {
+                    logger.LogDebug("Stopping enemy voice with janky code.");
+                    isDeadAnimationDone = true;
+                    creatureVoice.Stop();
+                    creatureVoice.PlayOneShot(dieSFX);
+                }
+                return;
+            }
+            timeSinceHittingLocalPlayer += Time.deltaTime;
+            ////timeSinceNewRandPos += Time.deltaTime;
+
+            var state = currentBehaviourStateIndex;
+            if (targetPlayer != null/* && (state == (int)State.StickingInFrontOfPlayer || state == (int)State.HeadSwingAttackInProgress)*/)
+            {
+                ////turnCompass.LookAt(targetPlayer.gameplayCamera.transform.position);
+                ////transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(0f, turnCompass.eulerAngles.y, 0f)), 4f * Time.deltaTime);
+            }
+            if (stunNormalizedTimer > 0f)
+            {
+                agent.speed = 0f;
+            }
+        }
+
+        public override void DoAIInterval()
+        {
+            return;
+            base.DoAIInterval();
+            if (isEnemyDead || StartOfRound.Instance.allPlayersDead)
+            {
+                return;
+            }
+
+            /*switch (base.currentBehaviourStateIndex)
+            {
+                case 0:
+                    base.agent.speed = 3f;
+                    if (FoundClosestPlayerInRange(25f, 3f))
+                    {
+                        LogIfDebugBuild("Start Target Player");
+                        this.StopSearch(base.currentSearch, true);
+                        this.SwitchToBehaviourClientRpc(1);
+                    }
+                    break;
+                case 1:
+                    base.agent.speed = 5f;
+                    if (!TargetClosestPlayerInAnyCase() || (Vector3.Distance(((Component)this).transform.position, ((Component)base.targetPlayer).transform.position) > 20f && !this.CheckLineOfSightForPosition(((Component)base.targetPlayer).transform.position, 45f, 60, -1f, (Transform)null)))
+                    {
+                        LogIfDebugBuild("Stop Target Player");
+                        this.StartSearch(((Component)this).transform.position, (AISearchRoutine)null);
+                        this.SwitchToBehaviourClientRpc(0);
+                    }
+                    else
+                    {
+                        StickingInFrontOfPlayer();
+                    }
+                    break;
+                case 2:
+                    break;
+                default:
+                    LogIfDebugBuild("This Behavior State doesn't exist!");
+                    break;
+            }*/
+        }
+
+        public override void OnCollideWithPlayer(Collider other)
+        {
+            logger.LogDebug("OnCollideWithPlayer");
+            return;
+            if (timeSinceHittingLocalPlayer < 1f)
+            {
+                return;
+            }
+            PlayerControllerB playerControllerB = MeetsStandardPlayerCollisionConditions(other);
+            if (playerControllerB != null)
+            {
+                logger.LogDebug("Enemy Collision with Player!");
+                timeSinceHittingLocalPlayer = 0f;
+                playerControllerB.DamagePlayer(20);
+            }
+        }
+
+        public override void HitEnemy(int force = 1, PlayerControllerB? playerWhoHit = null, bool playHitSFX = false, int hitID = -1)
+        {
+            base.HitEnemy(force, playerWhoHit, playHitSFX, hitID);
+            if (isEnemyDead)
+            {
+                return;
+            }
+            enemyHP -= force;
+            if (IsOwner)
+            {
+                if (enemyHP <= 0 && !isEnemyDead)
+                {
+                    // Our death sound will be played through creatureVoice when KillEnemy() is called.
+                    // KillEnemy() will also attempt to call creatureAnimator.SetTrigger("KillEnemy"),
+                    // so we don't need to call a death animation ourselves.
+
+                    ////StopCoroutine(SwingAttack());
+                    // We need to stop our search coroutine, because the game does not do that by default.
+                    StopCoroutine(searchCoroutine);
+                    KillEnemyOnOwnerClient();
+                }
+            }
+        }
     }
 }
